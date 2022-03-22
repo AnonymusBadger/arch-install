55639db80642e2245441ade50c863040c87b2016
Kajetan Orzełek <kajetan.orzelek@protonmail.com>
2022-03-22 19:18:45 +0100

    Remove the need of multiple password inputs

--- install/02-make-crypt.sh
+++ install/02-make-crypt.sh
@@ -3,16 +3,27 @@
 TOCRYPT=  # /dev/vda2 for qemu
 CRYPTNAME=
 
-# create crypt
 if [ -z "$TOCRYPT" ]; then
     read -r -p "Please choose a partition to create a new crypt: " TOCRYPT
 fi
 
-cryptsetup --cipher=aes-xts-plain64 --size=512 luksFormat "$TOCRYPT"
-
-# open it
 if [ -z "$CRYPTNAME" ]; then
     read -r -p "Please choose a new cryptname" CRYPTNAME
 fi
 
-cryptsetup open "$TOCRYPT" "$CRYPTNAME"
+while true; do
+    read -r -s -p "Insert password for the LUKS container: " password
+    while [ -z "$password" ]; do
+        echo
+        echo "You need to enter a password for the LUKS Container in order to continue."
+        read -r -s -p "Insert password for the LUKS container: " password
+        [ -n "$password" ] && break
+    done
+    echo
+    read -r -s -p "Password (again): " password2
+    echo
+    [ "$password" = "$password2" ] && break
+    echo "Passwords don't match, try again."
+done
+echo -n "$password" | --cipher=aes-xts-plain64 --size=512 luksFormat "$TOCRYPT"
+echo -n "$password" | cryptsetup open "$TOCRYPT" "$CRYPTNAME"
