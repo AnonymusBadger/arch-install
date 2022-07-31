278d88f71bb18f2f55272909aa4501b94064daf3
Kajetan Orzełek <kajetan.orzelek@protonmail.com>
2022-07-31 17:10:19 +0200

    Start script creation

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env -S bash -e
+
+# Clean the TTY
+clear
+
+# Update the live environment
+pacman -Syu
+
+# Load localized kayboard layout
+loadkeys pl
+setfont Lat2-Terminus16.psfu.gz -m 8859-2
+
+# Network config
+
+# Set up system clock
+timedatectl set-ntp true
+timedatectl set-timezone Europe/Warsaw
+
+### Drive and part creation ###
+clear
+# Selecting boot drive
+fdisk -l
+echo
+
+PS3="Select the drive "
+select ENTRY in $(lsblk -dpnoNAME | grep -P "/dev/sd|nvme|vd");
+do
+    DRIVE=$ENTRY
+    break
+done
