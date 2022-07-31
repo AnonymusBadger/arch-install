6ded872085967c7ad02185b52692902ab881b64e
Kajetan Orzełek <kajetan.orzelek@protonmail.com>
2022-07-31 17:41:20 +0200

    Add auto drive formating

--- install.sh
+++ install.sh
@@ -25,3 +25,30 @@ do
     DRIVE=$ENTRY
     break
 done
+
+# Wipe the drive
+echo "Wiping the drive"
+wipefs -af "$DRIVE" &>/dev/null
+sgdisk -Zo "$DRIVE" &>/dev/null
+# dd if=/dev/zero of=$DRIVE bs=4M status=progress
+
+# Creating a new partition scheme.
+echo "Creating new partition scheme on $DRIVE."
+parted -s "$DRIVE" \
+    mklabel gpt \
+    mkpart ESP fat32 1MiB 513MiB \
+    set 1 esp on \
+    mkpart primary 513MiB 100%
+
+ESP="/dev/disk/by-partlabel/ESP"
+PRIMARY="/dev/disk/by-partlabel/primary"
+
+echo "Informing the Kernel about the disk changes."
+partprobe "$DRIVE"
+
+# Formatting the ESP as VFAT.
+echo "Formatting the EFI Partition."
+mkfs.vfat -n EFI $ESP &>/dev/null
+
+echo "Formatting the Priamry Partition."
+mkfs.ext4 -L ARCH $PRIMARY &>/dev/null
