c38383a00295397e3d3c22531b213e9c39b869ad
Kajetan Orzełek <kajetan.orzelek@protonmail.com>
2022-08-01 22:07:15 +0200

    Finish v0.1

--- install.sh
+++ install.sh
@@ -56,7 +56,7 @@ echo "Formatting the EFI Partition."
 mkfs.vfat -n EFI $EFI &>/dev/null
 
 echo "Formatting the primary partition."
-mkfs.btrfs -L ROOT -n 32k -f $PRIMARY
+mkfs.btrfs -L ROOT -n 32k -f $PRIMARY &>/dev/null
 
 # Creating BTRFS subvolumes.
 echo "Creating BTRFS subvolumes."
@@ -65,7 +65,61 @@ btrfs sub create /mnt/@ &>/dev/null
 umount -R /mnt
 
 echo "Mounting the newly created subvolumes."
-mount -o $PRIMARY /mnt
+mount $PRIMARY /mnt
 mount -m $EFI /mnt/boot
 
 pacstrap /mnt base linux-zen intel-ucode linux-firmware sof-firmware grub efibootmgr neovim man-db man-pages texinfo sudo  btrfs-progs
+
+# Change pacman.conf
+sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 10/g' /mnt/etc/pacman.conf
+sed -i 's/#Color = 5/Color/g' /mnt/etc/pacman.conf
+
+# echo "Generating a new fstab."
+genfstab -U /mnt >>/mnt/etc/fstab
+
+# # Setting hostname.
+read -r -p "Please enter the hostname: " hostname
+echo "$hostname" >/mnt/etc/hostname
+
+# Setting hosts file.
+echo "Setting hosts file."
+cat >/mnt/etc/hosts <<EOF
+127.0.0.1   localhost
+::1         localhost
+127.0.1.1   $hostname.localdomain   $hostname
+EOF
+
+# Setting up locales.
+echo "Setting locale"
+sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /mnt/etc/pacman.conf
+sed -i 's/#pl_PL.UTF-8 UTF-8/pl_PL.UTF-8 UTF-8/g' /mnt/etc/pacman.conf
+echo "LANG=en_US.UTF-8" >/mnt/etc/locale.conf
+
+# Configuring the system.
+echo "Configuring chroot environment"
+arch-chroot /mnt /bin/bash -e <<EOF
+    # Setting up timezone.
+    ln -sf /usr/share/zoneinfo/Europe/Warsaw /etc/localtime &>/dev/null
+
+    # Setting up clock.
+    hwclock --systohc
+
+    # Generating locales.
+    echo "Generating locales."
+    locale-gen &>/dev/null
+
+    # Installing GRUB.
+    echo "Installing GRUB on /boot."
+    grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB  &>/dev/null
+
+    # Creating grub config file.
+    echo "Creating GRUB config file."
+    grub-mkconfig -o /boot/grub/grub.cfg &>/dev/null
+EOF
+
+# Setting root password.
+echo "Setting root password."
+arch-chroot /mnt /bin/passwd
+
+echo "Unmounting new filesystem"
+umount -R /mnt
