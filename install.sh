7e7227824c884eb32bbf270e9c48d29739226fb6
Kajetan Orzełek <kajetan.orzelek@protonmail.com>
2022-08-01 21:39:34 +0200

    Fix typo

--- install.sh
+++ install.sh
@@ -18,20 +18,20 @@ echo
 
 # Selecting the disk
 PS3="Select the disk "
-select drives in $(lsblk -dpnoNAME | grep -P "/dev/sd|nvme|vd");
+select drive in $(lsblk -dpnoNAME | grep -P "/dev/sd|nvme|vd");
 do
-    DISK=$ENTRY
+    DISK=$drive
     break
 done
 
 read -r -p "Secure wipe the drive before install? [y/N]? " response
 if [[ "${response,,}" =~ ^(yes|y)$ ]]; then
-    echo "Writing random bytes to the drive"
+    echo "Writing random bytes to $DISK"
     cryptsetup open --type plain -s 512 -c aes-xts-plain64 -d /dev/urandom $DISK to_be_wiped &>/dev/null
     dd bs=1M if=/dev/zero of=/dev/mapper/to_be_wiped status=progress
     cryptsetup close to_be_wiped &>/dev/null
 else
-    echo "Writing random bytes to the drive"
+    echo "Wiping $DISK"
     wipefs -af "$DISK" &>/dev/null
     sgdisk -Zo "$DISK" &>/dev/null
 fi
