4c680b1e10e787550b29f8c3979310ddc947ce23
Kajetan Orzełek <kajetan.orzelek@protonmail.com>
2022-08-01 23:08:20 +0200

    Reorganize

--- base-system/install.sh
+++ base-system/install.sh
@@ -74,6 +74,20 @@ pacstrap /mnt base linux-zen intel-ucode linux-firmware sof-firmware neovim man-
 echo "Generating fstab"
 genfstab -U /mnt >>/mnt/etc/fstab &>/dev/null
 
+# Network configuration
+echo "Configuring network"
+# Setting hostname.
+read -r -p "Please enter the hostname: " hostname
+echo "$hostname" >/etc/hostname
+
+# Setting hosts file.
+echo "Setting hosts file."
+cat >/etc/hosts <<EOF
+127.0.0.1   localhost
+::1         localhost
+127.0.1.1   $hostname.localdomain   $hostname
+EOF
+
 # Setting root password.
 echo "Setting root password."
 arch-chroot /mnt /bin/passwd
@@ -81,13 +95,31 @@ arch-chroot /mnt /bin/passwd
 # Create new user.
 read -r -p "Please enter name for a user account: " username
 arch-chroot /mnt /bin/bash -e <<EOF
+    echo "Setting up timezone"
+    # Setting up timezone.
+    ln -sf /usr/share/zoneinfo/Europe/Warsaw /etc/localtime &
+    >/dev/null
+
+    # Setting up clock.
+    hwclock --systohc
+
+    # Setting up locales.
+    echo "Setting up localization"
+    sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /mnt/etc/pacman.conf
+    sed -i 's/#pl_PL.UTF-8 UTF-8/pl_PL.UTF-8 UTF-8/g' /mnt/etc/pacman.conf
+    echo "LANG=en_US.UTF-8" >/mnt/etc/locale.conf
+
+    # Generating locales.
+    locale-gen &
+    >/dev/null
+
     echo "Adding $username with root privilege."
     useradd -m $username
     usermod -aG wheel $username
     usermod --shell /bin/zsh $username
 
     echo "Installing arch-install scripts"
-    git clone --depth=1 https://github.com/AnonymusBadger/arch-install.git ~$username
+    git clone --depth=1 https://github.com/AnonymusBadger/arch-install.git ~$username/arch-install
 EOF
 
 echo "Setting password for $username."
