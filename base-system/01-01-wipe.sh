1ddeac8fe583b5c06f586e5f4af1ed7110ab5c37
Kajetan Orzełek <kajetan.orzelek@protonmail.com>
2022-08-02 23:55:52 +0200

    Improve drive wiping

--- base-system/01-01-wipe.sh
+++ base-system/01-01-wipe.sh
@@ -7,8 +7,13 @@ if [[ "${response,,}" =~ ^(yes|y)$ ]]; then
     cryptsetup open --type plain -s 512 -c aes-xts-plain64 -d /dev/urandom "$DISK" to_be_wiped &>/dev/null
     dd bs=1M if=/dev/zero of=/dev/mapper/to_be_wiped status=progress
     cryptsetup close to_be_wiped &>/dev/null
+
+    echo "Wiping LUKS header"
+    cryptsetup erase "$DISK" &>/dev/null
+    wipefs -af "$DISK" &>/dev/null
 else
     echo "Wiping $DISK"
+    cryptsetup erase "$DISK" &>/dev/null
     wipefs -af "$DISK" &>/dev/null
     sgdisk -Zo "$DISK" &>/dev/null
 fi
