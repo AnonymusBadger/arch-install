749e8c292ef95e7c4d549a3110e160e0c0e69d02
Kajetan Orzełek <kajetan.orzelek@protonmail.com>
2022-08-03 00:56:41 +0200

    Fix path

--- post-scripts/00-base.sh
+++ post-scripts/00-base.sh
@@ -2,5 +2,5 @@
 
 # Change pacman.conf
 echo "Seting up pacman"
-sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 10/g' /mnt/etc/pacman.conf
-sed -i 's/#Color = 5/Color/g' /mnt/etc/pacman.conf
+sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 10/g' /etc/pacman.conf
+sed -i 's/#Color = 5/Color/g' /etc/pacman.conf
