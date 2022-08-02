efc5275c98af17c73b5b89f33705bc5d51213cac
Kajetan Orzełek <kajetan.orzelek@protonmail.com>
2022-08-03 00:55:38 +0200

    Install bootloader

---
+++ post-scripts/01-00-bootloader.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env -S bash -e
+
+bootctl install
+
+sed -i 's/HOOKS=(base udev autodetect modconf block filesystems keyboard fsck)/HOOKS=(base systemd autodetect keyboard sd-vconsole modconf block sd-encrypt filesystems fsck)/g'
+
+mkinitcpio -P
+
+{
+    echo "default  arch.conf"
+    echo "timeout  4"
+    echo "console-mode max"
+    echo "editor   no"
+} >/boot/efi/loader/loader.conf
+
+{
+    echo "title Arch Linux"
+    echo "linux /vmlinuz-linux"
+    echo "initrd /intel-ucode.img"
+    echo "initrd /initramfs-linux.img"
+    echo 'options root="LABEL=arch_os" rw'
+    echo "options rd.luks.name=c063888b-4066-4fd4-a9ec-47c2b8866c63=crypt rd.luks.options=discard root=/dev/mapper/crypt rw"
+} >/boot/efi/loader/entries/arch.conf
